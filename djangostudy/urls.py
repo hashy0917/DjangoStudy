c08467d5af27cbd141a83d21dc14f04446cab496
hashy0917 <oohasi3939@gmail.com>
2023-05-20 18:16:37 +0900

    commit

--- djangostudy/urls.py
+++ djangostudy/urls.py
@@ -1,7 +1,8 @@
-"""djangoStudying URL Configuration
+"""
+URL configuration for djangostudy project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,12 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from djangoStudying import views
+from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", views.index, name='index page'),
-    path("top/", views.top, name='test page'),
+    path('admin/', admin.site.urls),
+    path("polls/", include("polls.urls")),
 ]
